19cd8d5a26133a9d613c01833013476728699e28
toxic-dev0 <66657797+toxic-dev0@users.noreply.github.com>
2020-06-29 10:05:00 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -93,13 +93,13 @@ def paginate_help(page_number, loaded_plugins, prefix):
         "{} {}".format("♡", x),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
-    pairs = list(zip(modules[::number_of_cols], modules[2::number_of_cols]))
-    if len(modules) % number_of_cols == 2:
-        pairs.append((modules[-2],))
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
+    if len(modules) % number_of_cols == 1:
+        pairs.append((modules[-1],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 2)] + \
+        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
             (custom.Button.inline("Previous", data="{}_prev({})".format(prefix, modulo_page)),
              custom.Button.inline("Next", data="{}_next({})".format(prefix, modulo_page)))
